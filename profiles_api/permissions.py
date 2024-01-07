dc04575ff1f78d47b446e6dd294d0ff960ac8c64
Alexander Garber <clockworkpc@gmail.com>
2024-01-07 13:53:27 +0200

    search filter

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,16 @@ from rest_framework import permissions
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit his own profile"""
 
+    # correctly named method
     def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit his own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.id == request.user.id
+
+    # previously incorrectly named method (missing an underscore)
+    def has_objectpermission(self, request, view, obj):
         """Check user is trying to edit his own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
